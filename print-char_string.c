216f81bb2f026b058c29db037430b257e52e9ef7
Nimo08 <nimomohamed800@gmail.com>
2023-05-27 22:38:36 +0300

    Task 0

--- print-char_string.c
+++ print-char_string.c
@@ -13,7 +13,7 @@ int _printf(const char *format, ...)
 	va_list args;
 	int i = 0, num_chars = 0;
 	char c;
-	char *s;
+	const char *s;
 
 	va_start(args, format);
 	while (format[i] != '\0' && format != NULL)
@@ -26,11 +26,13 @@ int _printf(const char *format, ...)
 				case 'c':
 					c = (char)va_arg(args, int);
 					if (write(STDOUT_FILENO, &c, 1) < 0)
+					{
 						return (-1);
+					}
 					num_chars++;
 					break;
 				case 's':
-					s = va_arg(args, char *);
+					s = va_arg(args, const char *);
 					if (s == NULL)
 					{
 						write(STDOUT_FILENO, "(NULL)", 6);
