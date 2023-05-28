35683928603465332033b7b5afec445f9ff0e9c9
Nimo08 <nimomohamed800@gmail.com>
2023-05-28 11:48:19 +0300

    Task 0

--- print-char_string.c
+++ print-char_string.c
@@ -13,8 +13,10 @@ int _printf(const char *format, ...)
 	va_list args;
 	int i = 0, num_chars = 0;
 
+	if (format == NULL)
+		return (-1);
 	va_start(args, format);
-	while (format[i] != '\0' && format != NULL)
+	while (format[i] != '\0')
 	{
 		if (format[i] == '%')
 		{
@@ -31,10 +33,10 @@ int _printf(const char *format, ...)
 					num_chars += _printf_percent(args);
 					break;
 				default:
-					if (write(STDOUT_FILENO, &format[i - 1], 1) < 0)
+					if (write(STDOUT_FILENO, &format[i - 1], 1) == -1)
 						return (-1);
 					num_chars++;
-					if (write(STDOUT_FILENO, &format[i], 1) < 0)
+					if (write(STDOUT_FILENO, &format[i], 1) == -1)
 						return (-1);
 					num_chars++;
 					break;
@@ -42,7 +44,7 @@ int _printf(const char *format, ...)
 		}
 		else
 		{
-			if (write(STDOUT_FILENO, &format[i], 1) < 0)
+			if (write(STDOUT_FILENO, &format[i], 1) == -1)
 				return (-1);
 			num_chars++;
 		}
@@ -61,7 +63,7 @@ int _printf_char(va_list args)
 	char c;
 
 	c = (char)va_arg(args, int);
-	if (write(STDOUT_FILENO, &c, 1) < 0)
+	if (write(STDOUT_FILENO, &c, 1) == -1)
 		return (-1);
 	return (1);
 }
@@ -73,17 +75,20 @@ int _printf_char(va_list args)
 int _printf_string(va_list args)
 {
 	char *s;
-	int len;
+	int len = 0;
 
 	s = va_arg(args, char *);
 	if (s == NULL)
 	{
-		if (write(STDOUT_FILENO, "(NULL)", 6) < 0)
+		if (write(STDOUT_FILENO, "(NULL)", 6) == -1)
 			return (-1);
 		return (6);
 	}
-	len = strlen(s);
-	if (write(STDOUT_FILENO, s, len) < 0)
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	if (write(STDOUT_FILENO, s, len) == -1)
 		return (-1);
 	return (len);
 }
@@ -95,7 +100,7 @@ int _printf_string(va_list args)
 int _printf_percent(va_list args)
 {
 	(void) args;
-	if (write(STDOUT_FILENO, "%", 1) < 0)
+	if (write(STDOUT_FILENO, "%", 1) == -1)
 		return (-1);
 	return (1);
 }
